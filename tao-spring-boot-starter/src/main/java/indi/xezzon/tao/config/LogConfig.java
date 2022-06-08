fbdfc8ab5704d6bed3aabe2a1c2f5099c65bee70
jiangdingxuan <jiangdingxuan@rzdata.net>
2022-06-08 13:48:25 +0800

    修复 starter 作为依赖时，日志切面未生效的问题

---
+++ tao-spring-boot-starter/src/main/java/indi/xezzon/tao/config/LogConfig.java
@@ -0,0 +1,20 @@
+package indi.xezzon.tao.config;
+
+import indi.xezzon.tao.logger.LogRecordAspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnMissingBean(LogRecordAspect.class)
+public class LogConfig {
+
+  private static final Logger log = LoggerFactory.getLogger(LogConfig.class);
+
+  @Bean
+  public LogRecordAspect logRecordAspect() {
+    return new LogRecordAspect();
+  }
+}
