3ca4f54620ae6c30f2366407d1b041ea56503ab0
xezzon <xezzon@outlook.com>
2022-09-09 17:44:00 +0800

    细化全局异常并控制访问范围

--- tao-core/src/main/java/indi/xezzon/tao/exception/BaseException.java
+++ tao-core/src/main/java/indi/xezzon/tao/exception/BaseException.java
@@ -4,7 +4,7 @@ package indi.xezzon.tao.exception;
  * 业务异常基础类
  * @author xezzon
  */
-public class BaseException extends RuntimeException {
+public abstract class BaseException extends RuntimeException {
 
   @java.io.Serial
   private static final long serialVersionUID = 8565056568458668598L;
@@ -12,46 +12,18 @@ public class BaseException extends RuntimeException {
    * 错误码
    */
   private final String code;
-  /**
-   * 提示信息
-   */
-  private final String tip;
 
-  public BaseException() {
-    super();
-    this.code = null;
-    this.tip = null;
-  }
-
-  public BaseException(String message) {
+  protected BaseException(String code, String message) {
     super(message);
-    this.code = null;
-    this.tip = null;
+    this.code = code;
   }
 
-  public BaseException(Throwable cause) {
-    super(cause);
-    this.code = null;
-    this.tip = null;
-  }
-
-  public BaseException(String message, Throwable cause) {
-    super(message, cause);
-    this.code = null;
-    this.tip = null;
-  }
-
-  public BaseException(String code, String message, String tip, Throwable cause) {
+  protected BaseException(String code, String message, Throwable cause) {
     super(message, cause);
     this.code = code;
-    this.tip = tip;
   }
 
   public String getCode() {
     return this.code;
-  }
-
-  public String getTip() {
-    return this.tip;
   }
 }
