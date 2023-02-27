59e5afe1b5b12240f7044cca098d3f67bddab077
xezzon <xezzon@outlook.com>
2023-02-27 16:36:00 +0800

    定义多消息异常

---
+++ tao-core/src/main/java/io/github/xezzon/tao/exception/MultiException.java
@@ -0,0 +1,29 @@
+package io.github.xezzon.tao.exception;
+
+import java.util.Collection;
+
+/**
+ * 多消息异常
+ * @author xezzon
+ */
+public class MultiException extends RuntimeException {
+
+  @java.io.Serial
+  private static final long serialVersionUID = -2709278534007150462L;
+  private final String code;
+  private final Collection<String> messages;
+
+  public MultiException(Collection<String> messages, BaseException cause) {
+    super(cause);
+    this.code = cause.getCode();
+    this.messages = messages;
+  }
+
+  public String getCode() {
+    return this.code;
+  }
+
+  public Collection<String> getMessages() {
+    return this.messages;
+  }
+}
