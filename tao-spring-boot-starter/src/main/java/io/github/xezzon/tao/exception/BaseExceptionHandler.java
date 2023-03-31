8a0ef24d4b73fd131fd67a076bab1d0f13290bc4
xezzon <xezzon@outlook.com>
2023-03-31 10:24:25 +0800

    优化全局异常处理逻辑，异常返回消息交由调用方处理

--- tao-spring-boot-starter/src/main/java/io/github/xezzon/tao/exception/BaseExceptionHandler.java
+++ tao-spring-boot-starter/src/main/java/io/github/xezzon/tao/exception/BaseExceptionHandler.java
@@ -1,8 +1,6 @@
 package io.github.xezzon.tao.exception;
 
 import io.github.xezzon.tao.web.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,9 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class BaseExceptionHandler {
 
-  private static final Logger log = LoggerFactory.getLogger(BaseExceptionHandler.class);
+  protected final transient String errorMessage;
 
-  private static final String ERROR_MESSAGE = "无法响应您的操作，请稍后重试或联系客服。";
+  public BaseExceptionHandler() {
+    this("");
+  }
+
+  protected BaseExceptionHandler(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
 
   /**
    * 客户端异常
@@ -33,7 +37,7 @@ public class BaseExceptionHandler {
   @ExceptionHandler(ServerException.class)
   @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
   public Result<Void> handleServerException(ServerException e) {
-    return Result.fail(e.getCode(), ERROR_MESSAGE);
+    return Result.fail(e.getCode(), errorMessage);
   }
 
   /**
@@ -42,7 +46,7 @@ public class BaseExceptionHandler {
   @ExceptionHandler(ThirdPartyException.class)
   @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
   public Result<Void> handleThirdPartyException(ThirdPartyException e) {
-    return Result.fail(e.getCode(), ERROR_MESSAGE);
+    return Result.fail(e.getCode(), errorMessage);
   }
 
   /**
@@ -51,6 +55,6 @@ public class BaseExceptionHandler {
   @ExceptionHandler(MultiException.class)
   @ResponseStatus(code = HttpStatus.BAD_REQUEST)
   public Result<?> handleMultiException(MultiException e) {
-    return Result.fail(e.getCode(), null, e.getMessages());
+    return Result.fail(e.getCode(), errorMessage, e.getMessages());
   }
 }
