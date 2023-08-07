688f270fb5e67bb23391fa7c72383df30ce17145
xezzon <xezzon@outlook.com>
2023-08-07 12:03:47 +0800

    基于 micronaut-data-jpa 解析 CommonQuery

--- micronaut-tao/src/main/java/io/github/xezzon/tao/jpa/BaseJpaWrapper.java
+++ micronaut-tao/src/main/java/io/github/xezzon/tao/jpa/BaseJpaWrapper.java
@@ -1,12 +1,28 @@
 package io.github.xezzon.tao.jpa;
 
+import cn.hutool.core.util.ReflectUtil;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.EntityPathBase;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 import io.github.xezzon.tao.retrieval.CommonQuery;
 import io.micronaut.data.jpa.repository.JpaRepository;
 import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Id;
+import jakarta.transaction.Transactional;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author xezzon
@@ -45,11 +61,51 @@ public abstract class BaseJpaWrapper<
 
   @Override
   public Page<T> query(CommonQuery params) {
-    return null;
+    BooleanExpression queryClause =
+        JpaUtil.getQueryClause(params, this.getQuery(), this.getBeanClass());
+    Pageable pageable = JpaUtil.getPageable(params);
+    return this.findAll(queryClause, pageable);
   }
 
+  @SuppressWarnings({"rawtypes", "unchecked"})
   @Override
+  @Transactional(rollbackOn = {Exception.class})
   public boolean update(T t) {
-    return false;
+    JPAUpdateClause clause =
+        JpaUtil.getUpdateClause(t, getQueryFactory(), this.getQuery());
+    Field idField = Arrays.stream(this.getBeanClass().getDeclaredFields())
+        .filter(field -> field.isAnnotationPresent(Id.class))
+        .findAny()
+        .orElseThrow(() -> new RuntimeException("No identifier specified for entity"));
+    SimpleExpression column =
+        (SimpleExpression) ReflectUtil.getFieldValue(this.getQuery(), idField.getName());
+    Object value = ReflectUtil.getFieldValue(t, idField);
+    clause.where(column.eq(value));
+    long affected = clause.execute();
+    return affected > 0;
+  }
+
+  protected List<T> findAll(Predicate predicate) {
+    return this.createQuery(predicate, Pageable.unpaged()).fetch();
+  }
+
+  protected Page<T> findAll(Predicate predicate, Pageable pageable) {
+    JPAQuery<T> query = createQuery(predicate, Pageable.unpaged());
+    JPAQuery<T> pageQuery = createQuery(predicate, pageable);
+    return Page.of(pageQuery.fetch(), pageable, query.fetchCount());
+  }
+
+  protected JPAQuery<T> createQuery(@NotNull Predicate predicate, @NotNull Pageable pageable) {
+    return queryFactory.selectFrom(this.getQuery())
+        .where(predicate)
+        .orderBy(pageable.getOrderBy().stream()
+            .map((orderBy) -> new OrderSpecifier(
+                orderBy.isAscending() ? Order.ASC : Order.DESC,
+                Expressions.path(this.getQuery().getType(), this.getQuery(), orderBy.getProperty())
+            ))
+            .toArray(OrderSpecifier[]::new)
+        )
+        .offset(pageable.getOffset())
+        .limit(pageable.getSize());
   }
 }
